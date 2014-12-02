68cc7a35c2070262c8b91ba8461523f8984aa506
Daniel Fernández del Castillo Salazar <bonedark@gmail.com>
2014-12-02 20:15:16 +0000

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$servidor="localhost"; // Nuestro servidor  
-$database="people_proyecto"; // Nombre de nuestra base de datos  
-$password="230468"; // Nuestro password  
-$usuario="people_proyecto"; // Nombre de nuestro usuario  
-?>
