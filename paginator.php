96dd9630e5ae02245ffa6d1d7b0a8edb878c1532
Daniel <bonedark@gmail.com>
2014-12-12 00:01:05 +0000

    Refactorizando v2.2

---
+++ paginator.php
@@ -0,0 +1,32 @@
+<?php 
+function paginator($url,$start,$filas_tot,$aux) {
+if(($start + 5) <= $filas_tot)
+   {
+?>    <a href="javascript:Enviar('<?php echo $url; ?>?start=<?php echo ($start + 5); ?>','contenido');">Página anterior</a>
+	  
+      <?php
+   }
+
+   if($start > 0)
+   {
+	   ?>
+	<a href="javascript:Enviar('<?php echo $url; ?>?start=<?php echo ($start - 5); ?>','contenido');">Página siguiente</a>
+<?php
+   }
+
+ 
+    echo "<br>";
+   echo "<b>";
+   echo "Página: ";
+   echo "</b>";
+   $aux2= ($start/5) + 1;
+   echo"$aux2";
+   echo "<b>";
+   echo " de ";
+   echo "</b>";
+   echo "$aux";
+   echo "</br>";
+     echo "<br>";
+   echo "<b>";
+}
+   ?>
