4f38b9207c2264e5f46df97fa19408e7f033d836
Michelle9 <jhoanmary@gmail.com>
2014-12-14 02:01:50 +0000

    reset

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ $resultado2 = mysql_query ("SELECT * FROM comentarios order by fecha desc limit 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
 <title>People Revolution</title>
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
