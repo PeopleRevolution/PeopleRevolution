2bb4eac98ed0fe10b4abf478f498c039c269c365
Daniel <bonedark@gmail.com>
2014-12-15 02:08:58 +0000

    Modificando foro

--- foro/respuesta.php
+++ foro/respuesta.php
@@ -2,6 +2,7 @@
 require('funciones.php');
 $id = $_GET["id"];
 $citar = $_GET["citar"];
+$resp = $_GET["resp"];
 $row = array("id" => $id);
 if($citar==1)
 {
@@ -9,8 +10,15 @@ if($citar==1)
 	$sql = "SELECT titulo, mensaje, identificador AS id FROM foro WHERE id='$id'";
 	$rs = mysql_query($sql, $con);
 	if(mysql_num_rows($rs)==1) $row = mysql_fetch_assoc($rs);
-	$row["titulo"] = "Re: ".$row["titulo"];
-	$row["mensaje"] = "[citar]".$row["mensaje"]."[/citar]";
+	if($resp==1)
+	{
+		$row["titulo"] = $row["titulo"];
+		//$row["mensaje"] = "[citar]".$row["mensaje"]."[/citar]";
+	}
+	else{
+		$row["titulo"] = "Re: ".$row["titulo"];
+		$row["mensaje"] = "[citar]".$row["mensaje"]."[/citar]";
+	}
 	if($row["id"]==0) $row["id"]=$id;
 }
 $template = implode("", file('formulario.html'));
