59429e1a79c2fcc058b43f7918189c8d4d050ed4
Daniel <bonedark@gmail.com>
2014-12-16 13:02:03 +0000

    cambios config

--- foro/respuesta.php
+++ foro/respuesta.php
@@ -1,8 +1,9 @@
 <?php
-require('../config.php');
-require('funciones.php');
 require('../conexion.php');
 user_login();
+require('../config.php');
+require('funciones.php');
+
 $id = $_GET["id"];
 $citar = $_GET["citar"];
 $resp = $_GET["resp"];
