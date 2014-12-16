daf62967733f5349a7869e38b9b7d7ea5baf92b8
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-12-16 20:19:05 +0000

    Probando más tests en la librería

--- test/vendor/bin/iniciotest.php
+++ test/vendor/bin/iniciotest.php
@@ -5,14 +5,16 @@
  {
 	public function testPrueba()
 	{
+			$host = "https://api.twitter.com/1.1/";
 			$consumer_key = 12345;
 			$consumer_secret = 123456;
-		    $shouldreturn = 'https://api.twitter.com/oauth/access_token';
+		    $shouldreturn = "https://api.twitter.com/1.1/";
 			/*$twitter = TwitterOAuth;*/
 			$twitter = $this->getMockBuilder('TwitterOAuth')
 			->setConstructorArgs(array($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL))
 			->getMock();
-			$result = $twitter->accessTokenURL;
+			/*print_r(get_class_methods($twitter));*/
+			$result = $twitter->$host;
 			$this->assertEquals($shouldreturn, $result);
 	}
 }
