c81aaa8be7c27e693819b8b414cc8e773cb43000
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-12-16 13:56:07 +0000

    Creando tests para inicio.php

---
+++ test/vendor/bin/iniciotest.php
@@ -0,0 +1,13 @@
+<?php
+
+ namespace PeopleRevolution\test\vendor\bin;
+ 
+ use PeopleRevolution\inicio.php
+
+ class InicioTest extends \PHPUnit_Framework_TestCase
+ {
+	public function testPrueba()
+	{
+		$this-> assertTrue(true);
+	}
+}
