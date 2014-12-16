83f0e2d01160b6cf979a68d5f14c7331c07db248
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-12-16 19:58:20 +0000

    Modificando para pruebas en tests
    Nada nuevo bajo el sol

--- test/vendor/bin/iniciotest.php
+++ test/vendor/bin/iniciotest.php
@@ -1,13 +1,18 @@
 <?php
-
-	namespace PeopleRevolution\test\vendor\bin;
- 
-	use PeopleRevolution\inicio;
-
-	class InicioTest extends \PHPUnit_Framework_TestCase
+	include_once("../../../twitteroauth/twitteroauth.php");
+	
+ class PruebaTest extends PHPUnit_Framework_TestCase
+ {
+	public function testPrueba()
 	{
-		public function TestingFailConex()
-		{
-		$this-> assertTrue(true);
-		}
+			$consumer_key = 12345;
+			$consumer_secret = 123456;
+		    $shouldreturn = 'https://api.twitter.com/oauth/access_token';
+			/*$twitter = TwitterOAuth;*/
+			$twitter = $this->getMockBuilder('TwitterOAuth')
+			->setConstructorArgs(array($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL))
+			->getMock();
+			$result = $twitter->accessTokenURL;
+			$this->assertEquals($shouldreturn, $result);
 	}
+}
