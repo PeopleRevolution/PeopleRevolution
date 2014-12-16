424e054950a61808361d2af51eb153fef641081f
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-12-16 22:27:30 +0000

    Ya funciona el primer test añadido el segundo

--- test/vendor/bin/iniciotest.php
+++ test/vendor/bin/iniciotest.php
@@ -3,18 +3,29 @@
 	
  class PruebaTest extends PHPUnit_Framework_TestCase
  {
-	public function testPrueba()
+	public function accesstokenurltest()
 	{
 			
 			$consumer_key = 12345;
 			$consumer_secret = 123456;
-		    $shouldreturn = "https://api.twitter.com/1.1/";
-			/*$twitter = TwitterOAuth;*/
-			$twitter = $this->getMockBuilder('TwitterOAuth')
+		    $shouldreturn = 'https://api.twitter.com/oauth/access_token';
+			$twitter = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL);
+			/*$twitter = $this->getMockBuilder('TwitterOAuth')
 			->setConstructorArgs(array($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL))
 			->getMock();
 			/*print_r(get_class_methods($twitter));*/
-			$result = $host;
+			$result = $twitter->accessTokenURL();
+			$this->assertEquals($shouldreturn, $result);
+	}
+	
+	public function accesstokenurltest()
+	{
+			
+			$consumer_key = 12345;
+			$consumer_secret = 123456;
+		    $shouldreturn = 'https://api.twitter.com/oauth/authenticate';
+			$twitter = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token = NULL, $oauth_token_secret = NULL);
+			$result = $twitter->authenticateURL();
 			$this->assertEquals($shouldreturn, $result);
 	}
 }
