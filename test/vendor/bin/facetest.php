d007357ed1e205255bbb3a4ad2a5ac7c01e5928a
Santiago Sainz Fernández <evelpialight@gmail.com>
2014-12-17 16:31:07 +0000

    Añadidos test de facebook
    Problemas con las cookies

--- test/vendor/bin/facetest.php
+++ test/vendor/bin/facetest.php
@@ -12,7 +12,33 @@
 			'secret' => 'a5f6',  // Facebook App Secret
 			'cookie' => true,	
 			));
+			$result = $facebook->setPersistentData($not_key);
+			$this->assertEquals($shouldreturn, $result);
+	}
+	
+	public function testFailGetPersistentData()
+	{
+			$not_key = 123456;
+		    $shouldreturn = 'Unsupported key passed to getPersistentData.';
+			$facebook = new Facebook(array(
+			'appId'  => '1234',   // Facebook App ID 
+			'secret' => 'a5f6',  // Facebook App Secret
+			'cookie' => true,	
+			));
 			$result = $facebook->getPersistentData($not_key);
+			$this->assertEquals($shouldreturn, $result);
+	}
+	
+	public function testFailClearPersistentData()
+	{
+			$not_key = 123456;
+		    $shouldreturn = 'Unsupported key passed to clearPersistentData.';
+			$facebook = new Facebook(array(
+			'appId'  => '1234',   // Facebook App ID 
+			'secret' => 'a5f6',  // Facebook App Secret
+			'cookie' => true,	
+			));
+			$result = $facebook->clearPersistentData($not_key);
 			$this->assertEquals($shouldreturn, $result);
 	}
  }
