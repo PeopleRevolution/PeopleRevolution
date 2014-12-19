eba86413e03e6b606a9b311a1b796f3533c10e1d
Daniel <bonedark@gmail.com>
2014-12-19 09:38:42 +0000

    Arreglando buscar

--- buscar.php
+++ buscar.php
@@ -1,7 +1,6 @@
 ﻿<?php 
 @ob_start("ob_gzhandler");
 include_once("config.php");
-@mysql_query("SET NAMES 'utf8'"); 
 include("paginator.php");
 $buscar= $_GET['criterio'];
 $conex = mysql_connect ("$servidor","$usuario","$password"); 
@@ -47,7 +46,7 @@ if ($mostrador != ""){ $aux3 = "true";
               <div class="image"> <a href="javascript:Enviar('detalle.php?id=<?php echo $mostrador['id']; ?>','contenido');"><?php
                       echo "<img src=images_bd.php?id=$mostrador[id]&tam=2&aux=noticia alt=\"Imagen descriptiva del ultimo producto añadido\"  width=\"194\" height=\"99\"  class=alignleft >"; ?></a> </div>
               <div class="cnt">
-                <h4><a href="javascript:Enviar('detalle.php?id=<?php echo $mostrador['id']; ?>','contenido');"><?php echo $mostrador['titulo']; ?></a></h4>
+                <h4><a href="javascript:Enviar('detalle.php?id=<?php echo $mostrador['id']; ?>','contenido');"><?php echo utf8_encode($mostrador['titulo']); ?></a></h4>
                 <p><?php $aux = $mostrador['detalle']; 
                           $tam = strlen($aux);
                           
